f6d94ddfc6e0d6a5293be47b20cba0f587890424
hageduri <57488023+hageduri@users.noreply.github.com>
2024-04-13 05:00:13 +0530

    first commit
    created apicontroller1
    getData func
    data route
    test get method from postman

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\apiController1;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get("data",[apiController1::class,'getData']);
