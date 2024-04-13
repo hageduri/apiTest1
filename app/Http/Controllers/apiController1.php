f191238af781323f55931e52d61de04e811d50fc
hageduri <57488023+hageduri@users.noreply.github.com>
2024-04-13 20:51:18 +0530

    Validation before saving
    also commented the post route::add
    and its function add

--- app/Http/Controllers/apiController1.php
+++ app/Http/Controllers/apiController1.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\device;
+use Validator;
+use Illuminate\Validation\Rule;
 
 class apiController1 extends Controller
 {
@@ -14,21 +16,21 @@ class apiController1 extends Controller
         return $id?device::find($id):device::all();
     }
 
-    function add(Request $req){
+    // function add(Request $req){
 
-        $dev = new device();
-        $dev->name=$req->name;
-        $dev->member_id=$req->member_id;
-        $result=$dev->save();
+    //     $dev = new device();
+    //     $dev->name=$req->name;
+    //     $dev->member_id=$req->member_id;
+    //     $result=$dev->save();
 
-        if($result){
-            return ["Result"=>"Data has been saveds"];
-        }
+    //     if($result){
+    //         return ["Result"=>"Data has been saveds"];
+    //     }
 
-        else{
-            return ["Result"=>"Operation failed"];
-        }
-    }
+    //     else{
+    //         return ["Result"=>"Operation failed"];
+    //     }
+    // }
 
     function update(Request $req){
         $dev = device::find($req->id);
@@ -57,5 +59,39 @@ class apiController1 extends Controller
         else{
             return ["result"=>"delete operation failed"];
         }
+    }
+
+    function testData(Request $req){
+        $valid = array(
+            'name'=> [
+                'required',
+                'string',
+                'min:2',
+                'max:35',
+                Rule::unique('devices'),
+            ],
+            'member_id'=>[
+                'required',
+                'integer',
+                'min:1',
+                'max:200',
+                Rule::unique('devices'),
+            ],
+        );
+
+        $validator = Validator::make($req->all(),$valid);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(),401);
+        }
+        else{
+            $dev = new device();
+            $dev->name=$req->name;
+            $dev->member_id=$req->member_id;
+            $result=$dev->save();
+
+            return response()->json(['message' => 'Device created successfully'], 201);
+        }
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,8 @@ Route::get('/user', function (Request $request) {
 
 Route::get("data",[apiController1::class,'getData']);
 Route::get("list/{id?}",[apiController1::class,'list']);
-Route::post("add",[apiController1::class,'add']);
+// Route::post("add",[apiController1::class,'add']);
 Route::put("update",[apiController1::class,'update']);
 Route::get("search/{name}",[apiController1::class,'search']);
 Route::delete("delete/{id}",[apiController1::class,'delete']);
+Route::post("save",[apiController1::class,'testData']);
