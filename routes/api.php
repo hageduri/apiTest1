7331952c819b97f7fbd350d65b60053dddaf1d0c
hageduri <57488023+hageduri@users.noreply.github.com>
2024-04-14 02:58:29 +0530

    api group route sanctum updated

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,11 @@ use App\Http\Controllers\apiController1;
 use App\Http\Controllers\userController;
 
 Route::get('/user', function (Request $request) {
+    
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::get("data",[apiController1::class,'getData']);
     Route::get("list/{id?}",[apiController1::class,'list']);
     // Route::post("add",[apiController1::class,'add']);
@@ -13,15 +18,6 @@ Route::get('/user', function (Request $request) {
     Route::get("search/{name}",[apiController1::class,'search']);
     Route::delete("delete/{id}",[apiController1::class,'delete']);
     Route::post("save",[apiController1::class,'testData']);
-    // return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get("data",[apiController1::class,'getData']);
-Route::get("list/{id?}",[apiController1::class,'list']);
-// Route::post("add",[apiController1::class,'add']);
-Route::put("update",[apiController1::class,'update']);
-Route::get("search/{name}",[apiController1::class,'search']);
-Route::delete("delete/{id}",[apiController1::class,'delete']);
-Route::post("save",[apiController1::class,'testData']);
+});
 
 Route::post("login",[userController::class,'index']);
